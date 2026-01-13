131037ab623d2829e8ee1205d1730ca643fb2f23
Ghenghis <ghenghis@example.com>
2026-01-13 06:43:28 -0700

    Add System.Linq using to ChatbotService, FiddlerBridge, SettingsControlCenter

--- SvonyBrowser/Services/FiddlerBridge.cs
+++ SvonyBrowser/Services/FiddlerBridge.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.IO.Pipes;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using Newtonsoft.Json;
