19881bf097610693b1b29410288535a8cb271369
Ghenghis <ghenghis@example.com>
2026-01-13 07:02:47 -0700

    Add System.IO to MultiAccountOrchestrator, Linq and Helpers to LlmIntegrationService - 78 errors

--- SvonyBrowser/Services/LlmIntegrationService.cs
+++ SvonyBrowser/Services/LlmIntegrationService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Net.Http;
 using System.Net.WebSockets;
 using System.Runtime.CompilerServices;
@@ -9,6 +10,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using SvonyBrowser.Helpers;
 
 namespace SvonyBrowser.Services
 {
