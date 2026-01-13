88cf22592e03d976e5057aed8e3e2aa6ce925ad7
Ghenghis <ghenghis@example.com>
2026-01-13 06:37:11 -0700

    Fix more compilation errors: add using directives for Newtonsoft.Json, System.IO, System.Threading

--- SvonyBrowser/Controls/ChatbotPanel.xaml.cs
+++ SvonyBrowser/Controls/ChatbotPanel.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using Newtonsoft.Json;
 using SvonyBrowser.Services;
 
 namespace SvonyBrowser.Controls
--- SvonyBrowser/Services/CombatSimulator.cs
+++ SvonyBrowser/Services/CombatSimulator.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+
 namespace SvonyBrowser.Services
 {
 
